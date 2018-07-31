aa70beb10cf56f899df3822cb8907135d52bed92
Adam Urbina <adamurbina@gmail.com>
2018-07-31 21:25:59 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    def call(env)
+        request = Rack::Request.new(env)
+        response = Rack::Response.new
+
+        route = request.path.split("/")
+
+        response.finish
+    end
+    
 end
