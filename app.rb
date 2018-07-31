1ad05313f35806b4a779c678fba630fbcacb84af
Adam Urbina <adamurbina@gmail.com>
2018-07-31 21:34:07 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,8 +7,23 @@ class App < Sinatra::Base
         response = Rack::Response.new
 
         route = request.path.split("/").last
+        puts "route value:: >> #{route}"
+
+        response.write handle_request(route)
+        
 
         response.finish
+    end
+
+    def handle_request(route)
+        case route
+        when "name"
+            return "My name is Adam"
+        when "hometown"
+            return "My hometown is San Fracisco"
+        when "favorite-song"
+            return "California Love"
+        end
     end
 
 end
